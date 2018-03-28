3fb0963439264d4ffd9e6b1eb6310354b746d6da
Romeo Gatcheco III <romeogatcheco1207@gmail.com>
2018-03-29 00:25:52 +0800

    add particle collision and enemy health game program

---
+++ Assets/SCRIPTS/EnemyDamage.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+    public int _EnemyHealth;
+    public int _HalfLife;
+    public bool _isDamaged;
+    public bool _isDestroyed;
+    public ParticleSystem _Explosion;
+    public ParticleSystem _Damaged;
+    public ParticleSystem _EnemyBullet;
+    public AudioSource _DestroySound;
+	void Start () {
+        _isDamaged = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (!_isDestroyed) {
+            //normal bullet
+            if (other.gameObject.tag == "bullet")
+            {
+                _EnemyHealth--;
+            }
+
+            if (_EnemyHealth <= _HalfLife && !_isDamaged)
+            {
+                _Damaged.Play();
+                _isDamaged = true;
+            }
+
+            if (_EnemyHealth == 0)
+            {
+                Destroy(gameObject, 1f);
+                _Explosion.Play();
+                _EnemyBullet.Stop();
+                _DestroySound.Play();
+            }
+            //BOMB
+            if (other.gameObject.tag == "bomb")
+            {
+                Destroy(gameObject, 1f);
+            }
+        }
+    }
+}
