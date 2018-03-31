0f4c2a7a2c06d9a32eecd34d373fcbeecf84f348
Romeo Gatcheco III <romeogatcheco1207@gmail.com>
2018-04-01 01:37:26 +0800

    add minimap to UI

---
+++ Assets/SCRIPTS/UIMinimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMinimap : MonoBehaviour {
+public Quaternion iniRot;
+	// Use this for initialization
+	void Start () {
+        iniRot = transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = iniRot;
+    }
+}
+
